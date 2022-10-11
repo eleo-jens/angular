8095e36fab76af58069c7a6cf03bfb8f6efb8d09
Eleonore <eleonore.jens@gmail.com>
2022-10-11 10:11:22 +0200

    two-binding finished

--- src/app/demos/components/demo2/demo2.component.ts
+++ src/app/demos/components/demo2/demo2.component.ts
@@ -7,8 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Demo2Component implements OnInit {
   inputValue!: string;
+  numberValue!: number;
+  bool!: boolean;
 
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.inputValue = 'Valeurs par défaut';
+    this.numberValue = 42;
+    this.bool = false; //rajouter this pour dire qu'on va chercher la propriété de l'instance de classe
+  }
 }
